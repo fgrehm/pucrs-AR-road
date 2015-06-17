55615e35b51866f707e32fd8b00e7cd9a74d3417
Fabio Rehm <fgrehm@gmail.com>
2015-06-16 21:55:39 -0300

    Move away to multi marker example to use as a basis

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ LIB_DIR= /opt/ARToolKit/lib
 BIN_DIR= /opt/ARToolKit/bin
 
 LDFLAG=-pthread -lgstreamer-0.10 -lgobject-2.0 -lgmodule-2.0 -lgthread-2.0 -lrt -lxml2 -lglib-2.0 -L/usr/X11R6/lib -L/usr/local/lib -L$(LIB_DIR)
-LIBS= -lARgsub_lite -lARvideo -lAR -lpthread -lglut -lGLU -lGL -lXi -lX11 -lm -pthread -lgstreamer-0.10 -lgobject-2.0 -lgmodule-2.0 -lgthread-2.0 -lrt -lxml2 -lglib-2.0
+LIBS= -lARgsub -lARvideo -lARMulti -lAR -lpthread -lglut -lGLU -lGL -lXi -lX11 -lm -pthread -lgstreamer-0.10 -lgobject-2.0 -lgmodule-2.0 -lgthread-2.0 -lrt -lxml2 -lglib-2.0
 CFLAG= -O -pthread -I/usr/include/gstreamer-0.10 -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -I/usr/include/libxml2 -I/usr/X11R6/include -g -I$(INC_DIR)
 
 OBJS = scene.o
--- main.c
+++ main.c
@@ -1,409 +1,251 @@
-/*
- *  simpleLite.c
- *
- *  Some code to demonstrate use of gsub_lite's argl*() functions.
- *  Shows the correct GLUT usage to read a video frame (in the idle callback)
- *  and to draw it (in the display callback).
- *
- *  Press '?' while running for help on available key commands.
- *
- *  Copyright (c) 2001-2007 Philip Lamb (PRL) phil@eden.net.nz. All rights reserved.
- *
- *	Rev		Date		Who		Changes
- *	1.0.0	20040302	PRL		Initial version, simple test animation using GLUT.
- *	1.0.1	20040721	PRL		Correctly sets window size; supports arVideoDispOption().
- *
- */
-/*
- *
- * This file is part of ARToolKit.
- *
- * ARToolKit is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * ARToolKit is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with ARToolKit; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- */
-
-
-// ============================================================================
-//	Includes
-// ============================================================================
-
-#include <stdio.h>
-#include <stdlib.h>					// malloc(), free()
-#ifdef __APPLE__
-#  include <GLUT/glut.h>
-#else
-#  include <GL/glut.h>
-#endif
-#include <AR/config.h>
-#include <AR/video.h>
-#include <AR/param.h>			// arParamDisp()
-#include <AR/ar.h>
-#include <AR/gsub_lite.h>
-
-#include "scene.h"
-#include "globals.h"
-
-// ============================================================================
-//	Functions
-// ============================================================================
-
-static int setupCamera(const char *cparam_name, char *vconf, ARParam *cparam)
-{
-  ARParam			wparam;
-  int				xsize, ysize;
-
-  // Open the video path.
-  if (arVideoOpen(vconf) < 0) {
-    fprintf(stderr, "setupCamera(): Unable to open connection to camera.\n");
-    return (FALSE);
-  }
-
-  // Find the size of the window.
-  if (arVideoInqSize(&xsize, &ysize) < 0) return (FALSE);
-  fprintf(stdout, "Camera image size (x,y) = (%d,%d)\n", xsize, ysize);
-
-  // Load the camera parameters, resize for the window and init.
-  if (arParamLoad(cparam_name, 1, &wparam) < 0) {
-    fprintf(stderr, "setupCamera(): Error loading parameter file %s for camera.\n", cparam_name);
-    return (FALSE);
-  }
-  arParamChangeSize(&wparam, xsize, ysize, cparam);
-  fprintf(stdout, "*** Camera Parameter ***\n");
-  arParamDisp(cparam);
-
-  arInitCparam(cparam);
-
-  if (arVideoCapStart() != 0) {
-    fprintf(stderr, "setupCamera(): Unable to begin camera data capture.\n");
-    return (FALSE);
-  }
-
-  return (TRUE);
-}
-
-static int setupMarker(const char *patt_name, int *patt_id)
-{
-  // Loading only 1 pattern in this example.
-  if ((*patt_id = arLoadPatt(patt_name)) < 0) {
-    fprintf(stderr, "setupMarker(): pattern load error !!\n");
-    return (FALSE);
-  }
-
-  return (TRUE);
-}
-
-// Report state of ARToolKit global variables arFittingMode,
-// arImageProcMode, arglDrawMode, arTemplateMatchingMode, arMatchingPCAMode.
-static void debugReportMode(const ARGL_CONTEXT_SETTINGS_REF arglContextSettings)
-{
-  if (arFittingMode == AR_FITTING_TO_INPUT) {
-    fprintf(stderr, "FittingMode (Z): INPUT IMAGE\n");
-  } else {
-    fprintf(stderr, "FittingMode (Z): COMPENSATED IMAGE\n");
-  }
-
-  if (arImageProcMode == AR_IMAGE_PROC_IN_FULL) {
-    fprintf(stderr, "ProcMode (X)   : FULL IMAGE\n");
-  } else {
-    fprintf(stderr, "ProcMode (X)   : HALF IMAGE\n");
-  }
-
-  if (arglDrawModeGet(arglContextSettings) == AR_DRAW_BY_GL_DRAW_PIXELS) {
-    fprintf(stderr, "DrawMode (C)   : GL_DRAW_PIXELS\n");
-  } else if (arglTexmapModeGet(arglContextSettings) == AR_DRAW_TEXTURE_FULL_IMAGE) {
-    fprintf(stderr, "DrawMode (C)   : TEXTURE MAPPING (FULL RESOLUTION)\n");
-  } else {
-    fprintf(stderr, "DrawMode (C)   : TEXTURE MAPPING (HALF RESOLUTION)\n");
-  }
-
-  if (arTemplateMatchingMode == AR_TEMPLATE_MATCHING_COLOR) {
-    fprintf(stderr, "TemplateMatchingMode (M)   : Color Template\n");
-  } else {
-    fprintf(stderr, "TemplateMatchingMode (M)   : BW Template\n");
-  }
-
-  if (arMatchingPCAMode == AR_MATCHING_WITHOUT_PCA) {
-    fprintf(stderr, "MatchingPCAMode (P)   : Without PCA\n");
-  } else {
-    fprintf(stderr, "MatchingPCAMode (P)   : With PCA\n");
-  }
-}
-
-static void Quit(void)
-{
-  arglCleanup(gArglSettings);
-  arVideoCapStop();
-  arVideoClose();
-  exit(0);
-}
-
-static void Keyboard(unsigned char key, int x, int y)
-{
-  int mode;
-  switch (key) {
-    case 0x1B:						// Quit.
-    case 'Q':
-    case 'q':
-      Quit();
-      break;
-    case ' ':
-      gDrawRotate = !gDrawRotate;
-      break;
-    case 'C':
-    case 'c':
-      mode = arglDrawModeGet(gArglSettings);
-      if (mode == AR_DRAW_BY_GL_DRAW_PIXELS) {
-        arglDrawModeSet(gArglSettings, AR_DRAW_BY_TEXTURE_MAPPING);
-        arglTexmapModeSet(gArglSettings, AR_DRAW_TEXTURE_FULL_IMAGE);
-      } else {
-        mode = arglTexmapModeGet(gArglSettings);
-        if (mode == AR_DRAW_TEXTURE_FULL_IMAGE)	arglTexmapModeSet(gArglSettings, AR_DRAW_TEXTURE_HALF_IMAGE);
-        else arglDrawModeSet(gArglSettings, AR_DRAW_BY_GL_DRAW_PIXELS);
-      }
-      fprintf(stderr, "*** Camera - %f (frame/sec)\n", (double)gCallCountMarkerDetect/arUtilTimer());
-      gCallCountMarkerDetect = 0;
-      arUtilTimerReset();
-      debugReportMode(gArglSettings);
-      break;
-    case 'D':
-    case 'd':
-      arDebug = !arDebug;
-      break;
-    case '?':
-    case '/':
-      printf("Keys:\n");
-      printf(" q or [esc]    Quit demo.\n");
-      printf(" c             Change arglDrawMode and arglTexmapMode.\n");
-      printf(" d             Activate / deactivate debug mode.\n");
-      printf(" ? or /        Show this help.\n");
-      printf("\nAdditionally, the ARVideo library supplied the following help text:\n");
-      arVideoDispOption();
-      break;
-    default:
-      break;
-  }
-}
-
-static void Idle(void)
-{
-  static int ms_prev;
-  int ms;
-  float s_elapsed;
-  ARUint8 *image;
-
-  ARMarkerInfo    *marker_info;					// Pointer to array holding the details of detected markers.
-  int             marker_num;						// Count of number of markers detected.
-  int             j, k;
-
-  // Find out how long since Idle() last ran.
-  ms = glutGet(GLUT_ELAPSED_TIME);
-  s_elapsed = (float)(ms - ms_prev) * 0.001;
-  if (s_elapsed < 0.01f) return; // Don't update more often than 100 Hz.
-  ms_prev = ms;
-
-  // Update drawing.
-  DrawCubeUpdate(s_elapsed);
-
-  // Grab a video frame.
-  if ((image = arVideoGetImage()) != NULL) {
-    gARTImage = image;	// Save the fetched image.
-
-    gCallCountMarkerDetect++; // Increment ARToolKit FPS counter.
-
-    // Detect the markers in the video frame.
-    if (arDetectMarker(gARTImage, gARTThreshhold, &marker_info, &marker_num) < 0) {
-      exit(-1);
-    }
-
-    // Check through the marker_info array for highest confidence
-    // visible marker matching our preferred pattern.
-    k = -1;
-    for (j = 0; j < marker_num; j++) {
-      if (marker_info[j].id == gPatt_id) {
-        if (k == -1) k = j; // First marker detected.
-        else if(marker_info[j].cf > marker_info[k].cf) k = j; // Higher confidence marker detected.
-      }
-    }
-
-    if (k != -1) {
-      // Get the transformation between the marker and the real camera into gPatt_trans.
-      arGetTransMat(&(marker_info[k]), gPatt_centre, gPatt_width, gPatt_trans);
-      gPatt_found = TRUE;
-    } else {
-      gPatt_found = FALSE;
-    }
-
-    // Tell GLUT the display has changed.
-    glutPostRedisplay();
-  }
-}
-
-//
-//	This function is called on events when the visibility of the
-//	GLUT window changes (including when it first becomes visible).
-//
-static void Visibility(int visible)
-{
-  if (visible == GLUT_VISIBLE) {
-    glutIdleFunc(Idle);
-  } else {
-    glutIdleFunc(NULL);
-  }
-}
-
-//
-//	This function is called when the
-//	GLUT window is resized.
-//
-static void Reshape(int w, int h)
-{
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  glViewport(0, 0, (GLsizei) w, (GLsizei) h);
-
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
-
-  // Call through to anyone else who needs to know about window sizing here.
-}
-
-//
-// This function is called when the window needs redrawing.
-//
-static void Display(void)
-{
-  GLdouble p[16];
-  GLdouble m[16];
-
-  // Select correct buffer for this context.
-  glDrawBuffer(GL_BACK);
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the buffers for new frame.
-
-  arglDispImage(gARTImage, &gARTCparam, 1.0, gArglSettings);	// zoom = 1.0.
-  arVideoCapNext();
-  gARTImage = NULL; // Image data is no longer valid after calling arVideoCapNext().
-
-  // Projection transformation.
-  arglCameraFrustumRH(&gARTCparam, VIEW_DISTANCE_MIN, VIEW_DISTANCE_MAX, p);
-  glMatrixMode(GL_PROJECTION);
-  glLoadMatrixd(p);
-  glMatrixMode(GL_MODELVIEW);
-
-  // Viewing transformation.
-  glLoadIdentity();
-  // Lighting and geometry that moves with the camera should go here.
-  // (I.e. must be specified before viewing transformations.)
-  //none
-
-  if (gPatt_found) {
-
-    // Calculate the camera position relative to the marker.
-    // Replace VIEW_SCALEFACTOR with 1.0 to make one drawing unit equal to 1.0 ARToolKit units (usually millimeters).
-    arglCameraViewRH(gPatt_trans, m, VIEW_SCALEFACTOR);
-    glLoadMatrixd(m);
-
-    // All lighting and geometry to be drawn relative to the marker goes here.
-    if (!arDebug) {
-      glEnable(GL_DEPTH_TEST);
-      glDepthMask(GL_TRUE);
-      glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
-    }
-    DrawToten();
-    if (!arDebug) {
-      glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
-    }
-
-    DrawCube();
-    DrawSphere();
-
-  } // gPatt_found
-
-  // Any 2D overlays go here.
-  //none
-
-  glutSwapBuffers();
-}
-
-int main(int argc, char** argv)
-{
-  char glutGamemode[32];
-  const char *cparam_name = "Data/camera_para.dat";
-  //
-  // Camera configuration.
-  //
 #ifdef _WIN32
-  char			*vconf = "Data\\WDM_camera_flipV.xml";
-#else
-  char			*vconf = "";
+#include <windows.h>
 #endif
-  const char *patt_name  = "Data/patt.hiro";
+#include <stdio.h>
+#include <stdlib.h>
+#ifndef __APPLE__
+#include <GL/gl.h>
+#include <GL/glut.h>
+#else
+#include <OpenGL/gl.h>
+#include <GLUT/glut.h>
+#endif
+#include <AR/gsub.h>
+#include <AR/video.h>
+#include <AR/param.h>
+#include <AR/ar.h>
+#include <AR/arMulti.h>
 
-  // ----------------------------------------------------------------------------
-  // Library inits.
-  //
+/* set up the video format globals */
 
+#ifdef _WIN32
+char			*vconf = "Data\\WDM_camera_flipV.xml";
+#else
+char			*vconf = "";
+#endif
+
+int             xsize, ysize;
+int             thresh = 100;
+int             count = 0;
+
+char           *cparam_name    = "Data/camera_para.dat";
+ARParam         cparam;
+
+char                *config_name = "Data/multi/marker.dat";
+ARMultiMarkerInfoT  *config;
+
+static void   init(void);
+static void   cleanup(void);
+static void   keyEvent( unsigned char key, int x, int y);
+static void   mainLoop(void);
+static void   draw( double trans1[3][4], double trans2[3][4], int mode );
+
+
+int main(int argc, char **argv)
+{
   glutInit(&argc, argv);
+    init();
 
-  // ----------------------------------------------------------------------------
-  // Hardware setup.
-  //
-
-  if (!setupCamera(cparam_name, vconf, &gARTCparam)) {
-    fprintf(stderr, "main(): Unable to set up AR camera.\n");
-    exit(-1);
-  }
-
-  // ----------------------------------------------------------------------------
-  // Library setup.
-  //
-
-  // Set up GL context(s) for OpenGL to draw into.
-  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
-  if (!prefWindowed) {
-    if (prefRefresh) sprintf(glutGamemode, "%ix%i:%i@%i", prefWidth, prefHeight, prefDepth, prefRefresh);
-    else sprintf(glutGamemode, "%ix%i:%i", prefWidth, prefHeight, prefDepth);
-    glutGameModeString(glutGamemode);
-    glutEnterGameMode();
-  } else {
-    glutInitWindowSize(prefWidth, prefHeight);
-    glutCreateWindow(argv[0]);
-  }
-
-  // Setup argl library for current context.
-  if ((gArglSettings = arglSetupForCurrentContext()) == NULL) {
-    fprintf(stderr, "main(): arglSetupForCurrentContext() returned error.\n");
-    exit(-1);
-  }
-  debugReportMode(gArglSettings);
-  glEnable(GL_DEPTH_TEST);
-  arUtilTimerReset();
-
-  if (!setupMarker(patt_name, &gPatt_id)) {
-    fprintf(stderr, "main(): Unable to set up AR marker.\n");
-    Quit();
-  }
-
-  // Register GLUT event-handling callbacks.
-  // NB: Idle() is registered by Visibility.
-  glutDisplayFunc(Display);
-  glutReshapeFunc(Reshape);
-  glutVisibilityFunc(Visibility);
-  glutKeyboardFunc(Keyboard);
-
-  glutMainLoop();
-
+    arVideoCapStart();
+    argMainLoop( NULL, keyEvent, mainLoop );
   return (0);
+}
+
+static void   keyEvent( unsigned char key, int x, int y)
+{
+    /* quit if the ESC key is pressed */
+    if( key == 0x1b ) {
+        printf("*** %f (frame/sec)\n", (double)count/arUtilTimer());
+        cleanup();
+        exit(0);
+    }
+
+    if( key == 't' ) {
+        printf("*** %f (frame/sec)\n", (double)count/arUtilTimer());
+        printf("Enter new threshold value (current = %d): ", thresh);
+        scanf("%d",&thresh); while( getchar()!='\n' );
+        printf("\n");
+        count = 0;
+    }
+
+    /* turn on and off the debug mode with right mouse */
+    if( key == 'd' ) {
+        printf("*** %f (frame/sec)\n", (double)count/arUtilTimer());
+        arDebug = 1 - arDebug;
+        if( arDebug == 0 ) {
+            glClearColor( 0.0, 0.0, 0.0, 0.0 );
+            glClear(GL_COLOR_BUFFER_BIT);
+            argSwapBuffers();
+            glClear(GL_COLOR_BUFFER_BIT);
+            argSwapBuffers();
+        }
+        count = 0;
+    }
+
+}
+
+/* main loop */
+static void mainLoop(void)
+{
+    ARUint8         *dataPtr;
+    ARMarkerInfo    *marker_info;
+    int             marker_num;
+    double          err;
+    int             i;
+
+    /* grab a vide frame */
+    if( (dataPtr = (ARUint8 *)arVideoGetImage()) == NULL ) {
+        arUtilSleep(2);
+        return;
+    }
+    if( count == 0 ) arUtilTimerReset();
+    count++;
+
+    /* detect the markers in the video frame */
+    if( arDetectMarkerLite(dataPtr, thresh, &marker_info, &marker_num) < 0 ) {
+        cleanup();
+        exit(0);
+    }
+
+    argDrawMode2D();
+    if( !arDebug ) {
+        argDispImage( dataPtr, 0,0 );
+    }
+    else {
+        argDispImage( dataPtr, 1, 1 );
+        if( arImageProcMode == AR_IMAGE_PROC_IN_HALF )
+            argDispHalfImage( arImage, 0, 0 );
+        else
+            argDispImage( arImage, 0, 0);
+
+        glColor3f( 1.0, 0.0, 0.0 );
+        glLineWidth( 1.0 );
+        for( i = 0; i < marker_num; i++ ) {
+            argDrawSquare( marker_info[i].vertex, 0, 0 );
+        }
+        glLineWidth( 1.0 );
+    }
+
+    arVideoCapNext();
+
+    if( (err=arMultiGetTransMat(marker_info, marker_num, config)) < 0 ) {
+        argSwapBuffers();
+        return;
+    }
+    printf("err = %f\n", err);
+    if(err > 100.0 ) {
+        argSwapBuffers();
+        return;
+    }
+/*
+    for(i=0;i<3;i++) {
+        for(j=0;j<4;j++) printf("%10.5f ", config->trans[i][j]);
+        printf("\n");
+    }
+    printf("\n");
+*/
+    argDrawMode3D();
+    argDraw3dCamera( 0, 0 );
+    glClearDepth( 1.0 );
+    glClear(GL_DEPTH_BUFFER_BIT);
+    for( i = 0; i < config->marker_num; i++ ) {
+        if( config->marker[i].visible >= 0 ) draw( config->trans, config->marker[i].trans, 0 );
+        else                                 draw( config->trans, config->marker[i].trans, 1 );
+    }
+    argSwapBuffers();
+}
+
+static void init( void )
+{
+    ARParam  wparam;
+
+    /* open the video path */
+    if( arVideoOpen( vconf ) < 0 ) exit(0);
+    /* find the size of the window */
+    if( arVideoInqSize(&xsize, &ysize) < 0 ) exit(0);
+    printf("Image size (x,y) = (%d,%d)\n", xsize, ysize);
+
+    /* set the initial camera parameters */
+    if( arParamLoad(cparam_name, 1, &wparam) < 0 ) {
+        printf("Camera parameter load error !!\n");
+        exit(0);
+    }
+    arParamChangeSize( &wparam, xsize, ysize, &cparam );
+    arInitCparam( &cparam );
+    printf("*** Camera Parameter ***\n");
+    arParamDisp( &cparam );
+
+    if( (config = arMultiReadConfigFile(config_name)) == NULL ) {
+        printf("config data load error !!\n");
+        exit(0);
+    }
+
+    /* open the graphics window */
+    argInit( &cparam, 1.0, 0, 2, 1, 0 );
+    arFittingMode   = AR_FITTING_TO_IDEAL;
+    arImageProcMode = AR_IMAGE_PROC_IN_HALF;
+    argDrawMode     = AR_DRAW_BY_TEXTURE_MAPPING;
+    argTexmapMode   = AR_DRAW_TEXTURE_HALF_IMAGE;
+}
+
+/* cleanup function called when program exits */
+static void cleanup(void)
+{
+    arVideoCapStop();
+    arVideoClose();
+    argCleanup();
+}
+
+static void draw( double trans1[3][4], double trans2[3][4], int mode )
+{
+    double    gl_para[16];
+    GLfloat   mat_ambient[]     = {0.0, 0.0, 1.0, 1.0};
+    GLfloat   mat_ambient1[]    = {1.0, 0.0, 0.0, 1.0};
+    GLfloat   mat_flash[]       = {0.0, 0.0, 1.0, 1.0};
+    GLfloat   mat_flash1[]      = {1.0, 0.0, 0.0, 1.0};
+    GLfloat   mat_flash_shiny[] = {50.0};
+    GLfloat   mat_flash_shiny1[]= {50.0};
+    GLfloat   light_position[]  = {100.0,-200.0,200.0,0.0};
+    GLfloat   ambi[]            = {0.1, 0.1, 0.1, 0.1};
+    GLfloat   lightZeroColor[]  = {0.9, 0.9, 0.9, 0.1};
+
+    argDrawMode3D();
+    argDraw3dCamera( 0, 0 );
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+
+    /* load the camera transformation matrix */
+    glMatrixMode(GL_MODELVIEW);
+    argConvGlpara(trans1, gl_para);
+    glLoadMatrixd( gl_para );
+    argConvGlpara(trans2, gl_para);
+    glMultMatrixd( gl_para );
+
+    if( mode == 0 ) {
+        glEnable(GL_LIGHTING);
+        glEnable(GL_LIGHT0);
+        glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+        glLightfv(GL_LIGHT0, GL_AMBIENT, ambi);
+        glLightfv(GL_LIGHT0, GL_DIFFUSE, lightZeroColor);
+        glMaterialfv(GL_FRONT, GL_SPECULAR, mat_flash);
+        glMaterialfv(GL_FRONT, GL_SHININESS, mat_flash_shiny);
+        glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
+    }
+    else {
+        glEnable(GL_LIGHTING);
+        glEnable(GL_LIGHT0);
+        glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+        glLightfv(GL_LIGHT0, GL_AMBIENT, ambi);
+        glLightfv(GL_LIGHT0, GL_DIFFUSE, lightZeroColor);
+        glMaterialfv(GL_FRONT, GL_SPECULAR, mat_flash1);
+        glMaterialfv(GL_FRONT, GL_SHININESS, mat_flash_shiny1);
+        glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient1);
+    }
+    glMatrixMode(GL_MODELVIEW);
+    glTranslatef( 0.0, 0.0, 25.0 );
+    if( !arDebug ) glutSolidCube(50.0);
+     else          glutWireCube(50.0);
+    glDisable( GL_LIGHTING );
+
+    glDisable( GL_DEPTH_TEST );
 }
